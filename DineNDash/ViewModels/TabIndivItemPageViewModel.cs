1a411e4eb3f9616e7b7b4d5c9c1354b12a606985
RyanCorbin <ryancorbin3@gmail.com>
2018-11-27 21:47:36 -0800

    added menu items

--- DineNDash/ViewModels/TabIndivItemPageViewModel.cs
+++ DineNDash/ViewModels/TabIndivItemPageViewModel.cs
@@ -1,20 +1,79 @@
 ﻿using System;
-
+using System.Diagnostics;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
 using Xamarin.Forms;
+using DineNDash.Models;
+using DineNDash.Services;
+using System.Threading.Tasks;
+using Prism.Services;
 
 namespace DineNDash.ViewModels
 {
-    public class TabIndivItemPageViewModel : ContentPage
+    public class TabIndivItemPageViewModel : BindableBase, INavigationAware
     {
-        public TabIndivItemPageViewModel()
+        INavigationService nav_service;
+        IPageDialogService page_service;
+        IRepository _repo;
+
+        public DelegateCommand TapToOrder { get; set; }
+
+        private string place_order;
+        public string PlaceOrder
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Hello ContentPage" }
-                }
-            };
+            get { return place_order; }
+            set { SetProperty(ref place_order, value); }
         }
+
+        public TabIndivItemPageViewModel(INavigationService navigationService, IRepository repository, IPageDialogService pageDialogService)
+        {
+            nav_service = navigationService;
+            page_service = pageDialogService;
+            _repo = repository;
+
+            PlaceOrder = "Fries \n $1.80";
+
+            TapToOrder = new DelegateCommand(AddToCart);
+        }
+
+        private async void AddToCart()
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(AddToCart)}");
+
+            bool userResponse = await page_service.DisplayAlertAsync("Add Item?", "Are you sure you want to add item to cart?", "Ok", "Cancel");
+            if (userResponse == false)
+                return;
+            else
+            {
+                OrderItem newItem = new OrderItem
+                {
+                    Item = this.PlaceOrder
+                };
+
+                await _repo.AddItem(newItem);
+                var navParams = new NavigationParameters();
+                navParams.Add("ItemAdded", newItem);
+                await Task.Delay(1);
+            }
+
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnNavigatedFrom)}");
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnNavigatedTo)}");
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnNavigatingTo)}");
+        }
+
     }
 }
 
