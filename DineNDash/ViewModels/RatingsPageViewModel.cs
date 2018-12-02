73ae80c8f743720d375f7de8ed9f67767d57f1b3
RyanCorbin <ryancorbin3@gmail.com>
2018-12-02 12:00:17 -0800

    added device checker

--- DineNDash/ViewModels/RatingsPageViewModel.cs
+++ DineNDash/ViewModels/RatingsPageViewModel.cs
@@ -4,6 +4,8 @@ using Plugin.StoreReview;
 using Plugin.StoreReview.Abstractions;
 using Syncfusion.SfRating.XForms;
 using Xamarin.Forms;
+using Plugin.DeviceInfo;
+using Plugin.DeviceInfo.Abstractions;
 using System.Collections.ObjectModel;
 
 namespace DineNDash.ViewModels
@@ -13,7 +15,19 @@ namespace DineNDash.ViewModels
         
         public RatingsPageViewModel()
         {
-            string appId = "com.companyname.DineNDash";
+            var url = string.Empty;
+            var appId = string.Empty;
+
+            if (Plugin.DeviceInfo.CrossDeviceInfo.Current.Platform == Platform.iOS)
+            {
+                appId = "DineNDash";
+                url = $"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id={appId}&amp;onlyLatestVersion=true&amp;pageNumber=0&amp;sortOrdering=1&amp;type=Purple+Software";
+            }
+            else if (Plugin.DeviceInfo.CrossDeviceInfo.Current.Platform == Platform.Android)
+            {
+                appId = "com.companyname.DineNDash";
+                url = $"https://play.google.com/store/apps/details?id={appId}";
+            }
             Plugin.StoreReview.CrossStoreReview.Current.OpenStoreListing(appId);
         }
     }
